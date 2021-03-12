64e5b0c4613767713f3bb7ee52f9fd84f8b79652
Matty Wang <mattywang7@gmail.com>
2021-03-12 21:44:58 +0000

    part2 completed

---
+++ part2/expressions.h
@@ -0,0 +1,151 @@
+//
+// Created by Matty Wang on 11/03/2021.
+//
+
+#ifndef TEMPLATES_EXPRESSIONS_H
+#define TEMPLATES_EXPRESSIONS_H
+
+#endif //TEMPLATES_EXPRESSIONS_H
+
+#include <iostream>
+
+using namespace std;
+
+class OutOfRangeException : public exception {
+    const char *what() const noexcept override {
+        return "The input for x is not in the specified range.";
+    }
+};
+
+class DivideByZeroException : public exception {
+    const char *what() const noexcept override {
+        return "Division by zero is illegal.";
+    }
+};
+
+template<int LOWER, int UPPER>
+class Bound {
+public:
+    enum {
+        lowerBound = LOWER,
+        upperBound = UPPER
+    };
+};
+
+template<int N>
+class Literal {
+public:
+    static inline int eval(int x) {
+        return N;
+    }
+
+    enum {
+        lowerBound = N,
+        upperBound = N
+    };
+};
+
+template<class T>
+class Variable {
+public:
+    static inline int eval(int x) {
+        if (x < lowerBound || x > upperBound) {
+            throw OutOfRangeException();
+        } else {
+            return x;
+        }
+    }
+
+    enum {
+        lowerBound = T::lowerBound,
+        upperBound = T::upperBound
+    };
+};
+
+template<class A, class B>
+class Add {
+public:
+    static inline int eval(int x) {
+        return A::eval(x) + B::eval(x);
+    }
+
+    enum {
+        lowerBound = A::lowerBound + B::lowerBound,
+        upperBound = A::upperBound + B::upperBound
+    };
+};
+
+// loA <= A <= hiA
+// (loB <= B <= hiB) ------> (-hiB <= -B <= -loB)
+// loA - hiB <= A - B <= hiA - loB
+template<class A, class B>
+class Minus {
+public:
+    static inline int eval(int x) {
+        return A::eval(x) - B::eval(x);
+    }
+
+    enum {
+        lowerBound = A::lowerBound - B::upperBound,
+        upperBound = A::upperBound - B::lowerBound
+    };
+};
+
+// Helper functions for Times and Divide
+template<int A, int B>
+struct Min {
+    enum {
+        value = ((A < B) ? A : B)
+    };
+};
+
+template<int A, int B>
+struct Max {
+    enum {
+        value = ((A > B) ? A : B)
+    };
+};
+
+template<class A, class B>
+class Times {
+public:
+    static inline int eval(int x) {
+        return A::eval(x) * B::eval(x);
+    }
+
+    enum {
+        lowerBound = Min<Min<A::lowerBound * B::lowerBound, A::lowerBound * B::upperBound>::value, Min<A::upperBound * B::lowerBound, A::upperBound * B::upperBound>::value>::value,
+        upperBound = Max<Max<A::lowerBound * B::lowerBound, A::lowerBound * B::upperBound>::value, Max<A::upperBound * B::lowerBound, A::upperBound * B::upperBound>::value>::value
+    };
+};
+
+template<class A, class B>
+class Divide {
+public:
+    static inline int eval(int x) {
+        if (B::eval(x) == 0) {
+            throw DivideByZeroException();
+        } else {
+            return A::eval(x) / B::eval(x);
+        }
+    }
+
+    enum {
+        lowerBound = Min<Min<A::lowerBound / B::lowerBound, A::lowerBound / B::upperBound>::value, Min<A::upperBound / B::lowerBound, A::upperBound / B::upperBound>::value>::value,
+        upperBound = Max<Max<A::lowerBound / B::lowerBound, A::lowerBound / B::upperBound>::value, Max<A::upperBound / B::lowerBound, A::upperBound / B::upperBound>::value>::value
+    };
+};
+
+template<class T>
+class PrintBounds {
+public:
+    static inline int eval(int x) {
+        cout << T::lowerBound << " <= " << "The expression" << " <= " << T::upperBound << endl;
+        return T::eval(x);
+    }
+
+    enum {
+        lowerBound = T::lowerBound,
+        upperBound = T::upperBound
+    };
+};
--- part2/main.cpp
+++ part2/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include "expressions.h"
+
+const int variable = 3;
+const int outOfRange = 10;
+const int lowerBound = 0;
+const int upperBound = 5;
+
+// Add Test
+typedef PrintBounds<Add<Variable<Bound<lowerBound, upperBound>>, Literal<2>>> expressionAdd;
+
+// Minus Test
+typedef PrintBounds<Minus<Variable<Bound<lowerBound, upperBound>>, Literal<2>>> expressionMinus;
+
+// Times Test
+typedef PrintBounds<Times<Variable<Bound<lowerBound, upperBound>>, Literal<2>>> expressionTimes;
+
+// Divide Test
+typedef PrintBounds<Divide<Variable<Bound<lowerBound, upperBound>>, Literal<2>>> expressionDivide;
+
+// (x + 3) * (x + 5)
+typedef PrintBounds<Times<Add<Variable<Bound<lowerBound, upperBound>>, Literal<3>>, Add<Variable<Bound<lowerBound, upperBound>>, Literal<5>>>> expression;
+
+// OutOfRangeException Test
+typedef Variable<Bound<lowerBound, upperBound>> expressionOutOfRange;
 
 int main() {
-    std::cout << "part 2" << std::endl;
-    return 0;
+    cout << "Add Test:" << endl << expressionAdd::eval(variable) << endl << "------------" << endl;
+    cout << "Minus Test:" << endl << expressionMinus::eval(variable) << endl << "------------" << endl;
+    cout << "Times Test:" << endl << expressionTimes::eval(variable) << endl << "------------" << endl;
+    cout << "Divide Test:" << endl << expressionDivide::eval(variable) << endl << "------------" << endl;
+    cout << "(x_{0,5} + 3) * (x_{0,5} + 5): " << endl << expression::eval(variable) << endl << "------------" << endl;
+
+    cout << "Input x: 10 for range [0, 5]" << endl << expressionOutOfRange::eval(outOfRange) << endl;
 }

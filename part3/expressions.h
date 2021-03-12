8264899ea1b1f883ed376ab64c739712d5431253
Matty Wang <mattywang7@gmail.com>
2021-03-12 23:34:49 +0000

    part 3 finished

--- part3/expressions.h
+++ part3/expressions.h
@@ -6,3 +6,148 @@
 #define TEMPLATES_EXPRESSIONS_H
 
 #endif //TEMPLATES_EXPRESSIONS_H
+
+#include <iostream>
+
+using namespace std;
+
+int position = 0;
+
+class OutOfRangeException : public exception {
+    const char *what() const noexcept override {
+        return "The input is not in the specified range.";
+    }
+};
+
+class DivideByZeroException : public exception {
+    const char *what() const noexcept override {
+        return "Division by zero is illegal.";
+    }
+};
+
+template<int LOWER, int UPPER>
+class Bounds {
+public:
+    enum {
+        lowerBound = LOWER,
+        upperBound = UPPER
+    };
+};
+
+template<int N>
+class Literal {
+public:
+    static inline int eval(int variables[]) {
+        return N;
+    }
+
+    enum {
+        lowerBound = N,
+        upperBound = N
+    };
+};
+
+template<class T>
+class Variable {
+public:
+    static inline int eval(int variables[]) {
+        if (variables[position] < lowerBound || variables[position] > upperBound) {
+            throw OutOfRangeException();
+        } else {
+            return variables[position++];
+        }
+    }
+
+    enum {
+        lowerBound = T::lowerBound,
+        upperBound = T::upperBound
+    };
+};
+
+template<class A, class B>
+class Add {
+public:
+    static inline int eval(int variables[]) {
+        return A::eval(variables) + B::eval(variables);
+    }
+
+    enum {
+        lowerBound = A::lowerBound + B::lowerBound,
+        upperBound = A::upperBound + B::upperBound
+    };
+};
+
+template<class A, class B>
+class Minus {
+public:
+    static inline int eval(int variables[]) {
+        return A::eval(variables) - B::eval(variables);
+    }
+
+    enum {
+        lowerBound = A::lowerBound - B::upperBound,
+        upperBound = A::upperBound - B::lowerBound
+    };
+};
+
+// Helper functions for Times and Divide
+
+template<int A, int B>
+struct Min {
+    enum {
+        value = ((A < B) ? A : B)
+    };
+};
+
+template<int A, int B>
+struct Max {
+    enum {
+        value = ((A > B) ? A : B)
+    };
+};
+
+template<class A, class B>
+class Times {
+public:
+    static inline int eval(int variables[]) {
+        return A::eval(variables) * B::eval(variables);
+    }
+
+    enum {
+        lowerBound = Min<Min<A::lowerBound * B::lowerBound, A::lowerBound * B::upperBound>::value, Min<A::upperBound * B::lowerBound, A::upperBound * B::upperBound>::value>::value,
+        upperBound = Max<Max<A::lowerBound * B::lowerBound, A::lowerBound * B::upperBound>::value, Max<A::upperBound * B::lowerBound, A::upperBound * B::upperBound>::value>::value
+    };
+};
+
+template<class A, class B>
+class Divide {
+public:
+    static inline int eval(int variables[]) {
+        if (B::eval(variables) == 0) {
+            throw DivideByZeroException();
+        } else {
+            return A::eval(variables) / B::eval(variables);
+        }
+    }
+
+    enum {
+        lowerBound = Min<Min<A::lowerBound / B::lowerBound, A::lowerBound / B::upperBound>::value, Min<A::upperBound / B::lowerBound, A::upperBound / B::upperBound>::value>::value,
+        upperBound = Max<Max<A::lowerBound / B::lowerBound, A::lowerBound / B::upperBound>::value, Max<A::upperBound / B::lowerBound, A::upperBound / B::upperBound>::value>::value
+    };
+};
+
+// Each time call PrintBounds, the position will be reset to 0.
+template<class T>
+class PrintBounds {
+public:
+    static inline int eval(int variables[]) {
+        position = 0;
+        cout << T::lowerBound << " <= " << "The expression" << " <= " << T::upperBound << endl;
+        return T::eval(variables);
+    }
+
+    enum {
+        lowerBound = T::lowerBound,
+        upperBound = T::upperBound
+    };
+};
--- part3/main.cpp
+++ part3/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include "expressions.h"
+
+// x + (y - 2) * (z - 3)
+
+// input arrays
+int variables_1[3] = {3, 13, 23};
+int variables_2[3] = {5, 15, 25};
+int variables_3[3] = {7, 17, 27};
+
+// bounds for each variable
+const int lower_bound_1 = 0;
+const int upper_bound_1 = 9;
+const int lower_bound_2 = 10;
+const int upper_bound_2 = 19;
+const int lower_bound_3 = 20;
+const int upper_bound_3 = 29;
+
+typedef PrintBounds<Add<Variable<Bounds<lower_bound_1, upper_bound_1>>,
+                        Times<Minus<Variable<Bounds<lower_bound_2, upper_bound_2>>, Literal<2>>,
+                              Minus<Variable<Bounds<lower_bound_3, upper_bound_3>>, Literal<3>>>>> expression;
 
 int main() {
-    std::cout << "part 3" << std::endl;
+    int outputs[3];
+    outputs[0] = expression::eval(variables_1);
+    outputs[1] = expression::eval(variables_2);
+    outputs[2] = expression::eval(variables_3);
+    for (auto output : outputs) {
+        cout << output << endl;
+    }
+
+    // Test OutOfRangeException
+    variables_2[1] = 21; // y is out of range now
+    cout << expression::eval(variables_2) << endl;
     return 0;
 }
